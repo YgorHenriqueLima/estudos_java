e247c591479ef9e0a14bdab0c1fa2f62eddd3e50
Ygorhenrique <118612433+YgorHenriqueLima@users.noreply.github.com>
2025-03-13 12:10:20 -0300

    .

--- NetBeansProjects/OperadoresSwing/src/com/ygorlima/operadores/TelaValores.java
+++ NetBeansProjects/OperadoresSwing/src/com/ygorlima/operadores/TelaValores.java
@@ -133,22 +133,16 @@ public class TelaValores extends javax.swing.JFrame {
                     break;
                 }
             }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(TelaValores.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(TelaValores.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(TelaValores.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(TelaValores.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
+        
+        //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new TelaValores().setVisible(true);
-            }
+        java.awt.EventQueue.invokeLater(() -> {
+            new TelaValores().setVisible(true);
         });
     }
 
