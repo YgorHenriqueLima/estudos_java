13f667b7b4337ad7e83a690fc18d810fe6361fbe
Ygorhenrique <118612433+YgorHenriqueLima@users.noreply.github.com>
2025-03-14 20:40:24 -0300

    adicionando novos exercicios de Java

---
+++ NetBeansProjects/EstruturaDoWhile/src/main/java/com/mycompany/estruturadowhile/EstruturaDoWhile.java
@@ -0,0 +1,11 @@
+package com.mycompany.estruturadowhile;
+public class EstruturaDoWhile {
+
+    public static void main(String[] args) {
+        int cc = 0;
+        do{
+            System.out.println("cambalhota");
+            cc++;
+        }while(cc < 4);
+    }
+}
