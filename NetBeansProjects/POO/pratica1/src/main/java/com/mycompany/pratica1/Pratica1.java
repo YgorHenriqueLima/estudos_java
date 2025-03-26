46fb6fbe27c88a191c3b20fc4b8730fe388a2232
Ygorhenrique <118612433+YgorHenriqueLima@users.noreply.github.com>
2025-03-26 17:33:09 -0300

    .

--- NetBeansProjects/POO/pratica1/src/main/java/com/mycompany/pratica1/Pratica1.java
+++ NetBeansProjects/POO/pratica1/src/main/java/com/mycompany/pratica1/Pratica1.java
@@ -1,16 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.pratica1;
-
-/**
- *
- * @author ygor
- */
 public class Pratica1 {
-
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Lutador l[] = new Lutador[6];
+        
+        l[0] = new Lutador("Pretty Boy", "Franca", 31, 1.75f, 68.9f, 11, 2, 1);        
+        l[1] = new Lutador("PutsScript", "Brasil", 29, 1.68f, 57.8f, 14, 2, 3);
+        l[2] = new Lutador("SnapShadow", "EUA", 35 , 1.65f , 80.9f, 12, 2 ,1);
+        l[3] = new Lutador("DeadCode","Austrália", 28 ,1.93f ,81.6f, 13,0,2);
+        l[4] = new Lutador("UFOCobol", "Brasil", 37, 1.70f, 119.3f, 5, 4, 3);
+        l[5] = new Lutador("Nerdaart", "EUA",30, 1.81f, 105.7f, 12,2,4);
     }
 }
